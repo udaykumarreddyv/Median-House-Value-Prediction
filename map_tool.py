ba6c5623b5ee0fb1cd21686fe2ae93f4e74021ad
Cameron Lewis <ctl051@ucsd.edu>
2021-11-15 09:55:31 -0800

    Map tool upload
    Added data folder to gitignore to stop data from being pushed to github
    
    geo_json_data.py contains information on how to load geo_jsons for use
    in our map tool
    
    zipcode_mapping.py generates and stores the mapping between our zillow
    dataset zipcodes and the actual zipcodes based on looking up zipcodes by
    the lat long of each section
    
    map_tool is used to overlay geojsons on a map and create a heatmap over
    the data to convey information.

---
+++ map_tool.py
@@ -0,0 +1,52 @@
+from geo_json_data import geo_json_data
+import pathlib
+import folium  # Version 0.12.0
+
+
+class Map:
+    '''
+    Use this to make visualizations on a geo_json based dataset
+    '''
+
+    def __init__(self, geojson_file_path=str(r'./data/State-zip-code-GeoJSON/')):
+        assert isinstance(geojson_file_path, (pathlib.Path, str)),"geojson fiel path must be appropriate type"
+        if isinstance(geojson_file_path, str):
+            geojson_file_path = pathlib.Path(geojson_file_path)
+        assert geojson_file_path.exists(),"file path must exist"+str(geojson_file_path)
+        if not geojson_file_path.is_file():
+            self.geodata = geo_json_data(fname=list(geojson_file_path.glob('*.json')))
+        else:
+            self.geodata = geo_json_data(fname=geojson_file_path)
+        pass
+
+    def make_map(self, listOfMaps,mapTitle):
+        '''
+        make and save the folium map
+        :param listOfMaps: list[list[str,pd.DataFrame,zipcodeColumn,valueColumn]] unique zipcodes
+        :return:
+        '''
+        assert isinstance(listOfMaps,list)
+        m = folium.Map(location=[48, -102], zoom_start=3)
+
+        # This assumes that the ordering in zipcodes is consistent (which it is because they are ordered lists
+        for mapping in listOfMaps:
+            assert isinstance(mapping,list)
+            layerTitle = mapping[0]
+            filteredGeodata = self.geodata[mapping[1].keys()]
+            mapVals = [mapping[1][i] for i in filteredGeodata.zipcodes]
+
+            folium.Choropleth(
+                # geo_data=geodata[zhZips['regionidzip'].unique()].dataJson,
+                geo_data=filteredGeodata.dataJson,
+                name="choropleth",
+                data=mapVals,
+                columns=[mapping[3], mapping[4]],
+                key_on="feature.properties.ZCTA5CE10",
+                fill_color="YlGn",
+                fill_opacity=0.7,
+                line_opacity=0.2,
+                legend_name=layerTitle,
+            ).add_to(m)
+
+            folium.LayerControl().add_to(m)
+        m.save(mapTitle)
---
+++ zipcode_mapping.py
@@ -0,0 +1,74 @@
+import geopy # Version 2.2.0
+from time import sleep
+import pickle
+import pandas as pd
+import pathlib
+
+
+class Zipcode_Mapping:
+    '''
+    Use this class to load and save the zipcode mapping from the zillow dataset to
+    example usage:
+    zm = Zipcode_Mapping()
+    zm.create_mapping(zhLatLong)
+    zm.save_mapping()
+    zm.zipcodes
+    '''
+
+    def __init__(self):
+        '''
+        load constants for internal use
+        '''
+        self._geolocator = geopy.Nominatim(user_agent='zillow-data-validation')
+        self.zipcodes = None
+        pass
+
+    def _get_zipcode(self, df):
+        '''
+        query Nominatim API via geopy to get closest zipcode to the lat long calculated
+        :param df: pd.DataFrame with columns ['latitude','longitude'] and row index: 'regionidzip'
+        '''
+        # Due to Nominatim TOS we must not query more than once per second
+        sleep(1.01)
+        location = self._geolocator.reverse((df['latitude'], df['longitude']))
+        if ((location is not None) and
+                (location.raw is not None) and
+                ('address' in location.raw.keys()) and
+                ('postcode' in location.raw['address'].keys())):
+            return ((df.name), int(location.raw['address']['postcode'][:5]))
+        else:
+            return (df.name, None)
+
+    def save_mapping(self, fname="zipcode_mapping.pickle"):
+        '''
+        :param fname: name of new file
+        '''
+        assert isinstance(fname, (pathlib.Path, str))
+        if isinstance(fname, str):
+            fname = pathlib.Path(fname)
+        with open(fname, 'wb') as f:
+            pickle.dump(self.zipcodes, f)
+
+        pass
+
+    def create_mapping(self, df):
+        '''
+        :param df: pd.DataFrame with columns ['latitude','longitude'] and row index: 'regionidzip'
+        '''
+        assert isinstance(df, pd.DataFrame)
+        self.zipcodes = df.apply(self._get_zipcode, axis=1)
+        self.zipcodes = {i[0]: i[1] for i in self.zipcodes.values}
+
+    def load_mapping(self, fname="zipcode_mapping.pickle"):
+        '''
+        warning, the fname file will be unpickled. this data can come from anywhere and can be malicious
+        The dict is of the form WrongZip:RealZip
+        :param fname: name of pickle file
+        '''
+        assert isinstance(fname, (pathlib.Path, str))
+        if isinstance(fname, str):
+            fname = pathlib.Path(fname)
+
+        if fname.exists():
+            with open(fname, 'rb') as f:
+                self.zipcodes = pickle.load(f)
