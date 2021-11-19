7169f37fb8b63acd7db54ab99ae5f3f7b07f720f
Uday Varikuti <udaykumar0496@gmail.com>
2021-11-19 11:02:23 -0800

    Function to convert datatypes of the input dataframe Reducing the memory footprint by converting float64 to float32, and int64 to int32 datatypes respectively

--- data_processing.py
+++ data_processing.py
@@ -19,7 +19,7 @@ def clean_data(fname, plots=False):
     pd.options.display.float_format = '{:,.3f}'.format
     data = pd.read_csv('data/properties_2016.csv')
     data.columns = [column.strip() for column in data.columns]  #clean data colunm names
-    
+
     #find missing data info
     missing_df = show_missing_value_stats(data, showplot=plots)
     filtered_cols = missing_df[missing_df['percent_missing'] > MISSING_PERCENT_THRESHOLD].field.tolist()
@@ -54,9 +54,28 @@ def clean_data(fname, plots=False):
         'buildingqualitytypeid', 'unitcnt', 'propertyzoningdesc'])
 
     data.drop(drop_columns, axis=1, inplace=True)
+    data = change_df_dtypes(data)
+
     #remove rows with null values
     return data.dropna(how='any',axis=0)
 
+def change_df_dtypes(df):
+    '''
+    Fixes datatype issues in the dataframe
+    df : dataframe
+    returns: A new dataframe with fixed datatypes
+    '''
+    from sklearn import preprocessing
+    for c, dtype in zip(df.columns, df.dtypes):
+        if dtype == 'float64':
+            df[c] = df[c].astype(np.float32) 
+        elif dtype == 'int64':
+            df[c] = df[c].astype(np.int32)
+        elif dtype == 'object':
+            label = preprocessing.LabelEncoder()
+            label.fit(list(df[c].values))
+            df[c] = label.transform(list(df[c].values))
+    return df
 
 # Function to analyse missing values. To be used later
 def show_missing_value_stats(df, showplot=False):
